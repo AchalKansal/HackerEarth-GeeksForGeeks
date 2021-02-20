a99aee3c4c8b68129a94c5b2461be3eddaf9bb76
i321962 <achal.kansal@sap.com>
2021-02-20 18:32:56 +0530

    format

--- src/main/java/com/sap/gfg/leet/code/AddTwoNumbersInList.java
+++ src/main/java/com/sap/gfg/leet/code/AddTwoNumbersInList.java
@@ -1,8 +1,5 @@
 package com.sap.gfg.leet.code;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 
  * 
@@ -25,36 +22,38 @@ import java.util.List;
 public class AddTwoNumbersInList {
 
 	public static void main(String[] args) {
-		ListNode l1 = new ListNode(1,null);
+		ListNode l1 = new ListNode(1, null);
 		ListNode l2 = new ListNode(2, l1);
 		ListNode l3 = new ListNode(3, l2);
-		
-		ListNode l4 = new ListNode(1,null);
+
+		ListNode l4 = new ListNode(1, null);
 		ListNode l5 = new ListNode(2, l4);
 		ListNode l6 = new ListNode(3, l5);
-		
-		addTwoNumbers(l3,l6);
+
+		addTwoNumbers(l3, l6);
 	}
 
 	public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-		
+
 		ListNode dummyHead = new ListNode(0);
-	    ListNode p = l1, q = l2, curr = dummyHead;
-	    int carry = 0;
-	    while (p != null || q != null) {
-	        int x = (p != null) ? p.val : 0;
-	        int y = (q != null) ? q.val : 0;
-	        int sum = carry + x + y;
-	        carry = sum / 10;
-	        curr.next = new ListNode(sum % 10);
-	        curr = curr.next;
-	        if (p != null) p = p.next;
-	        if (q != null) q = q.next;
-	    }
-	    if (carry > 0) {
-	        curr.next = new ListNode(carry);
-	    }
-	    return dummyHead.next;
+		ListNode p = l1, q = l2, curr = dummyHead;
+		int carry = 0;
+		while (p != null || q != null) {
+			int x = (p != null) ? p.val : 0;
+			int y = (q != null) ? q.val : 0;
+			int sum = carry + x + y;
+			carry = sum / 10;
+			curr.next = new ListNode(sum % 10);
+			curr = curr.next;
+			if (p != null)
+				p = p.next;
+			if (q != null)
+				q = q.next;
+		}
+		if (carry > 0) {
+			curr.next = new ListNode(carry);
+		}
+		return dummyHead.next;
 
 	}
 }
