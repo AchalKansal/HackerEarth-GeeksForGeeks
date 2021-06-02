86801bda16a0a9303025edd9f943e8fe5d9a9380
Achal <achalkansal.ak@gmail.com>
2021-06-02 13:14:30 +0530

    duplicateZeros

--- src/main/java/com/sap/gfg/prac/Anagram.java
+++ src/main/java/com/sap/gfg/prac/Anagram.java
@@ -19,85 +19,102 @@ import java.util.HashMap;
 //are Anagram or not using HashMap 
 
 
-class Anagram { 
+class Anagram {
 
-	// Function to check whether two strings 
-	// are an anagram of each other 
-	static boolean areAnagram(String str1, String str2) 
-	{ 
+    private static int CHARACTER_RANGE = 256;
 
-		HashMap<Character, Integer> hmap1 
-			= new HashMap<Character, Integer>(); 
-		HashMap<Character, Integer> hmap2 
-			= new HashMap<Character, Integer>(); 
+    // Function to check whether two strings
+    // are an anagram of each other
+    static boolean areAnagram ( String str1, String str2 ) {
 
-		char arr1[] = str1.toCharArray(); 
-		char arr2[] = str2.toCharArray(); 
+        HashMap<Character, Integer> hmap1
+                = new HashMap<Character, Integer> ();
+        HashMap<Character, Integer> hmap2
+                = new HashMap<Character, Integer> ();
 
-		// Mapping first string 
-		for (int i = 0; i < arr1.length; i++) { 
+        char arr1[] = str1.toCharArray ();
+        char arr2[] = str2.toCharArray ();
 
-			if (hmap1.get(arr1[i]) == null) { 
+        // Mapping first string
+        for ( int i = 0; i < arr1.length; i++ ) {
 
-				hmap1.put(arr1[i], 1); 
-			} 
-			else { 
-				Integer c = (int)hmap1.get(arr1[i]); 
-				hmap1.put(arr1[i], ++c); 
-			} 
-		} 
+            if (hmap1.get ( arr1[ i ] ) == null) {
 
-		// Mapping second String 
-		for (int j = 0; j < arr2.length; j++) { 
+                hmap1.put ( arr1[ i ], 1 );
+            } else {
+                Integer c = (int) hmap1.get ( arr1[ i ] );
+                hmap1.put ( arr1[ i ], ++c );
+            }
+        }
 
-			if (hmap2.get(arr2[j]) == null) 
-				hmap2.put(arr2[j], 1); 
-			else { 
+        // Mapping second String
+        for ( int j = 0; j < arr2.length; j++ ) {
 
-				Integer d = (int)hmap2.get(arr2[j]); 
-				hmap2.put(arr2[j], ++d); 
-			} 
-		} 
+            if (hmap2.get ( arr2[ j ] ) == null)
+                hmap2.put ( arr2[ j ], 1 );
+            else {
 
-		if (hmap1.equals(hmap2)) 
-			return true; 
-		else
-			return false; 
-	} 
+                Integer d = (int) hmap2.get ( arr2[ j ] );
+                hmap2.put ( arr2[ j ], ++d );
+            }
+        }
 
-	// Test function 
-	public static void test(String str1, String str2) 
-	{ 
+        if (hmap1.equals ( hmap2 ))
+            return true;
+        else
+            return false;
+    }
 
-		System.out.println("Strings to be checked are:\n"
-						+ str1 + "\n" + str2 + "\n"); 
+    public static boolean isAnagramCounting ( String string1, String string2 ) {
+        if (string1.length () != string2.length ()) {
+            return false;
+        }
+        int count[] = new int[ CHARACTER_RANGE ];
+        for ( int i = 0; i < string1.length (); i++ ) {
+            count[ string1.charAt ( i ) ]++;
+            count[ string2.charAt ( i ) ]--;
+        }
+        for ( int i = 0; i < CHARACTER_RANGE; i++ ) {
+            if (count[ i ] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-		// Find the result 
-		if (areAnagram(str1, str2)) 
-			System.out.println("The two strings are "
-							+ "anagrams of each other\n"); 
-		else
-			System.out.println("The two strings are not"
-							+ " anagrams of each other\n"); 
-	} 
+    // Test function
+    public static void test ( String str1, String str2 ) {
 
-	// Driver program 
-	public static void main(String args[]) 
-	{ 
+        System.out.println ( "Strings to be checked are:\n"
+                + str1 + "\n" + str2 + "\n" );
 
-		// Get the Strings 
-		String str1 = "geeksforgeeks"; 
-		String str2 = "forgeeksgeeks"; 
+        // Find the result
+        if (areAnagram ( str1, str2 ))
+            System.out.println ( "The two strings are "
+                    + "anagrams of each other\n" );
+        else
+            System.out.println ( "The two strings are not"
+                    + " anagrams of each other\n" );
+    }
 
-		// Test the Strings 
-		//test(str1, str2); 
+    // Driver program
+    public static void main ( String args[] ) {
 
-		// Get the Strings 
-		str1 = "geeksforgeeks"; 
-		str2 = "geeks"; 
+        // Get the Strings
+        String str1 = "geeksforgeeks";
+        String str2 = "forgeeksgeeks";
 
-		// Test the Strings 
-		test(str1, str2); 
-	} 
+        // Test the Strings
+        //test(str1, str2);
+
+		isAnagramCounting ( str1, str2 );
+        // Get the Strings
+        str1 = "geeksforgeeks";
+        str2 = "geeks";
+
+
+        // Test the Strings
+        test ( str1, str2 );
+    }
 } 
 
