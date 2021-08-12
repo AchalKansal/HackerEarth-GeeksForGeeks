5358ca970f148765ba3cb0c0df4670f5338220b2
Achal <achalkansal.ak@gmail.com>
2021-08-12 14:06:07 +0530

    Update ShallowCloing.java

--- src/main/java/com/learn/concepts/cloning/ShallowCloing.java
+++ src/main/java/com/learn/concepts/cloning/ShallowCloing.java
@@ -1,4 +1,37 @@
 package com.learn.concepts.cloning;
 
 public class ShallowCloing {
+  private String firstName;
+  private String LastName;
+  private int age;
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return LastName;
+  }
+
+  public void setLastName(String lastName) {
+    LastName = lastName;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public ShallowCloing(String firstName, String lastName, int age) {
+    this.firstName = firstName;
+    LastName = lastName;
+    this.age = age;
+  }
 }
