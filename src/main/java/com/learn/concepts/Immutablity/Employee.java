28e5981d8e8279bb67de0c42baa1c5a204937c2b
Achal <achalkansal.ak@gmail.com>
2021-08-12 13:38:32 +0530

    Update Employee.java

--- src/main/java/com/learn/concepts/Immutablity/Employee.java
+++ src/main/java/com/learn/concepts/Immutablity/Employee.java
@@ -5,17 +5,18 @@ import java.io.Serializable;
 /**
  * The class must be declared as final (So that child classes can’t be created)
  *
- * Data members in the class must be declared as private (So that direct access is not allowed)
+ * <p>Data members in the class must be declared as private (So that direct access is not allowed)
  *
- * Data members in the class must be declared as final (So that we can’t change the value of it after object creation)
+ * <p>Data members in the class must be declared as final (So that we can’t change the value of it
+ * after object creation)
  *
- * A parameterized constructor should initialize all the fields performing a deep copy (So that data
- *  members can’t be modified with object reference)
+ * <p>A parameterized constructor should initialize all the fields performing a deep copy (So that
+ * data members can’t be modified with object reference)
  *
- * Deep Copy of objects should be performed in the getter methods (To return a copy rather
- *  than returning the actual object reference)
+ * <p>Deep Copy of objects should be performed in the getter methods (To return a copy rather than
+ * returning the actual object reference)
  *
- * No setters (To not have the option to change the value of the instance variable)
+ * <p>No setters (To not have the option to change the value of the instance variable)
  */
 public final class Employee implements Serializable {
 
@@ -44,6 +45,6 @@ public final class Employee implements Serializable {
 
   public static void main(String[] args) {
     Employee emp = new Employee("Achal", "Kansal", 28);
-    System.out.println(emp.getFirstName ());
+    System.out.println(emp.getFirstName());
   }
 }
