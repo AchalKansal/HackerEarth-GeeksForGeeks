c8d9ae614976f80497e6b1108f1433d91f122a04
Achal <achalkansal.ak@gmail.com>
2021-08-12 13:37:39 +0530

    Update Employee.java

--- src/main/java/com/learn/concepts/Immutablity/Employee.java
+++ src/main/java/com/learn/concepts/Immutablity/Employee.java
@@ -2,6 +2,21 @@ package com.learn.concepts.Immutablity;
 
 import java.io.Serializable;
 
+/**
+ * The class must be declared as final (So that child classes can’t be created)
+ *
+ * Data members in the class must be declared as private (So that direct access is not allowed)
+ *
+ * Data members in the class must be declared as final (So that we can’t change the value of it after object creation)
+ *
+ * A parameterized constructor should initialize all the fields performing a deep copy (So that data
+ *  members can’t be modified with object reference)
+ *
+ * Deep Copy of objects should be performed in the getter methods (To return a copy rather
+ *  than returning the actual object reference)
+ *
+ * No setters (To not have the option to change the value of the instance variable)
+ */
 public final class Employee implements Serializable {
 
   private static final long serialVersionUID = 12345L;
